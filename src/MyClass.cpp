67bbe14e36bd9ef4d7c2094010a4bd0201085e56
admin <nazik@nazik.myguest.virtualbox.org>
2024-12-14 13:53:20 +0200

    Completed FuncA implementation; added comments.

--- src/MyClass.cpp
+++ src/MyClass.cpp
@@ -1,6 +1,11 @@
 #include "MyClass.h"
 #include <cmath>
-
-double MyClass::FuncA(double x) {
-    return std::tanh(x);
+double MyClass::FuncA(double x, int n) {
+    double result = 0.0;
+    for (int k = 1; k <= n; ++k) {
+        double term = (2.0 * std::pow(x, 2*k-1)) / ((2*k) * (2*k-1));
+        result += term;
+    }
+    return result;
 }
+
