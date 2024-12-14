7d7c4d8288742c3bdf5aad01d6f8b0f7f18cad59
admin <nazik@nazik.myguest.virtualbox.org>
2024-12-14 14:18:45 +0200

    Intermediate changes in branchA before switching to master

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 
 int main() {
     MyClass obj;
-    double result = obj.FuncA(0);
-    std::cout << "The result of FuncA(0) is: " << result << std::endl;
+    int n = 5; // Кількість елементів ряду
+    double result = obj.FuncA(0, n);
+    std::cout << "The result of FuncA(0) for n=" << n << " is: " << result << std::endl;
     return 0;
 }
+
